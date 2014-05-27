e6a5ded8d533cdd6805ab9b0551f08a0e3316e6a
Roberts <crobert1@L000900581.EHEALTH.HPHC.ORG>
2014-05-27 12:04:11 -0400

    initial version

---
+++ src/main/java/com/opencover/App.java
@@ -0,0 +1,13 @@
+package com.opencover;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
